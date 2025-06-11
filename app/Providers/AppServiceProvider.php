eb31396ce588ef42f505b6857d435c5fd4e561c0
aqilahnabilah <aqilahnabilah.hilmy@gmail.com>
2025-06-11 15:18:13 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Fallback untuk Vite jika manifest tidak ditemukan
+        Vite::useCspNonce('vite-nonce');
+        
+        // Fallback jika manifest tidak ditemukan
+        Vite::useIntegrityKey('integrity');
+        
+        // Tambahkan hot file jika dalam mode development
+        if (file_exists(public_path('hot'))) {
+            Vite::useHotFile('hot');
+        }
     }
 }
